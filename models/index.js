76d24132f4ec00ed8cabeac899e35cca008dafad
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-06-08 08:16:25 -0700

    Modified models/index.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var db        = {vyqkr29pb9cez6zf};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
