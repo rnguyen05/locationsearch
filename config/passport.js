a08fa77743c91878f86a977f3f3845c5909dea3e
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-06-14 18:37:56 -0700

    M

--- config/passport.js
+++ config/passport.js
@@ -44,7 +44,7 @@ passport.use(new LocalStrategy(
 passport.use(new FacebookStrategy({
 	clientID: "202065647087620",
 	clientSecret: "77c213b28e805272002ed465854d3cca",
-	callbackURL: 'http://localhost:3000/login/facebook/return',
+	callbackURL: 'https://locationsearch-prj2.herokuapp.com/',
 	profileFields:['id','displayName','emails']
 	}, function(accessToken, refreshToken, profile, done) {
 		console.log(profile);
